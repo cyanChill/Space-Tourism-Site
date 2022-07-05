ccd49d53868edd4bb828a843ed7efd117d4095bb
cyanChill <anthonyliang9@gmail.com>
2022-07-05 19:56:46 -0400

    added routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { Routes, Route } from "react-router-dom";
+
+import MainNav from "./components/navigation";
+import HomePage from "./pages/home";
+import DestinationPage from "./pages/destination";
+import CrewPage from "./pages/crew";
+import TechnologyPage from "./pages/technology";
+
 const App = () => {
-  return <div>My App</div>;
+  return (
+    <>
+      <MainNav />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="destination" element={<DestinationPage />} />
+        <Route path="crew" element={<CrewPage />} />
+        <Route path="technology" element={<TechnologyPage />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
