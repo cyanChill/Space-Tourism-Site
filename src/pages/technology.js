6b28e3fc2152241207521f7dc643500c6dc7a685
cyanChill <anthonyliang9@gmail.com>
2022-07-06 13:30:25 -0400

    translated & improved original html to react

--- src/pages/technology.js
+++ src/pages/technology.js
@@ -5,14 +5,62 @@ import styles from "../styles/technology.module.css";
 
 const TechnologyPage = () => {
   const [subPg, setSubPg] = useState(0);
-  const [data] = useState(Data.technology);
+  const [currData] = useState(Data.technology);
 
   /* Set Background Image */
   useEffect(() => {
     document.body.classList = "technology";
   }, []);
 
-  return <div>Technology Page</div>;
+  return (
+    <section className={`flex ${styles["content-wrapper"]}`}>
+      <h1 className={styles["pg-info"]}>
+        <span>03</span> Space launch 101
+      </h1>
+
+      <div className={`flex ${styles["content"]}`}>
+        <div className={`flex ${styles["tech-img"]}`}>
+          <picture>
+            <source
+              srcSet={currData[subPg].images.portrait}
+              media="(min-width: 768px)"
+            />
+            <img
+              src={currData[subPg].images.landscape}
+              alt={`${currData[subPg].name}`}
+            />
+          </picture>
+        </div>
+
+        <div className={`flex ${styles["nonImg-content"]}`}>
+          <div className={`flex ${styles["tech-switcher"]}`}>
+            {currData.map((_, idx) => (
+              <div
+                key={idx}
+                className={`circle-btn outline ${
+                  subPg === idx ? "active" : undefined
+                } ${styles["custom-circle"]}`}
+                onClick={() => setSubPg(idx)}
+              >
+                {idx}
+              </div>
+            ))}
+          </div>
+
+          <div className={`flex ${styles["main-content"]}`}>
+            <h2 className={`flex ${styles["tech-info"]}`}>
+              <span>The terminology...</span>
+              <span>{currData[subPg].name}</span>
+            </h2>
+
+            <main className={styles.description}>
+              {currData[subPg].description}
+            </main>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default TechnologyPage;
